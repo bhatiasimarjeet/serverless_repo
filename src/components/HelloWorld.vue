54a510c1a408349ffbecf40f04c37dc256a542e1
Simarjeet Singh Bhatia <96399536+bhatiasimarjeet@users.noreply.github.com>
2023-12-21 23:00:31 +0530

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://3fn0jelpjb.execute-api.us-east-1.amazonaws.com/default/function-1'
+const API_ENDPOINT = ' https://uzdega3dkj.execute-api.us-east-1.amazonaws.com/default/lambda1forserverless'
 
 export default {
   name: 'app',
